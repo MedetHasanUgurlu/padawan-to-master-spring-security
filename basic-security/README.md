75e8064e8b7c8dbf2f5db5efa5a4ca63add00697
MedetHasanUgurlu <mdt.hsn.76@hotmail.com>
2023-06-01 12:32:43 +0300

    basic security #2

--- basic-security/README.md
+++ basic-security/README.md
@@ -4,4 +4,6 @@
 <div align="center">
 <img src="img.png">
 <img src="img_1.png">
+<img src="img_2.png">
+<img src="img_3.png">
 </div>
--- basic-security/src/main/java/com/medron/basicsecurity/BasicSecurityApplication.java
+++ basic-security/src/main/java/com/medron/basicsecurity/BasicSecurityApplication.java
@@ -1,13 +1,17 @@
 package com.medron.basicsecurity;
 
+import org.apache.tomcat.util.http.parser.Authorization;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.web.access.intercept.AuthorizationFilter;
 
 @SpringBootApplication
 public class BasicSecurityApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BasicSecurityApplication.class, args);
+
+
     }
 
 }
