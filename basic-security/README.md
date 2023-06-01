4eb9d6b19e2b420da410140a3b3464402fa569e2
MedetHasanUgurlu <mdt.hsn.76@hotmail.com>
2023-06-01 11:47:22 +0300

    basic security #1

---
+++ basic-security/README.md
@@ -0,0 +1,7 @@
+
+# BASIC SECURITY
+
+<div align="center">
+<img src="img.png">
+<img src="img_1.png">
+</div>
---
+++ basic-security/src/main/java/com/medron/basicsecurity/TestController.java
@@ -0,0 +1,14 @@
+package com.medron.basicsecurity;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+    @GetMapping
+    public String test(){
+        return "Test";
+    }
+}
